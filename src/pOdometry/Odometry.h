0d9cebc5e1292b9283df6e2982e3fd7b521ccdcd
kiriakos2004 <kiriakosal2004@yahoo.gr>
2026-06-15 16:09:42 +0300

    added basic folder stracture

---
+++ src/pOdometry/Odometry.h
@@ -0,0 +1,36 @@
+/************************************************************/
+/*    NAME: Kiriakos Alexiou                                              */
+/*    ORGN: MIT, Cambridge MA                               */
+/*    FILE: Odometry.h                                          */
+/*    DATE: December 29th, 1963                             */
+/************************************************************/
+
+#ifndef Odometry_HEADER
+#define Odometry_HEADER
+
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+
+class Odometry : public AppCastingMOOSApp
+{
+ public:
+   Odometry();
+   ~Odometry();
+
+ protected: // Standard MOOSApp functions to overload  
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+
+ protected: // Standard AppCastingMOOSApp function to overload 
+   bool buildReport();
+
+ protected:
+   void registerVariables();
+
+ private: // Configuration variables
+
+ private: // State variables
+};
+
+#endif 
